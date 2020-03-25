81525dcae414b3099c3d7971cfecd763666f9856
Oleksiy Yankovskyy <oleksiy.web@gmail.com>
2020-03-25 21:52:57 +0200

    Added size and type validators for the image uploading

--- file_upload_app/forms.py
+++ file_upload_app/forms.py
@@ -12,4 +12,3 @@ class AvatarForm(forms.ModelForm):
     class Meta:
         model = AvatarImage
         fields = ['image',]
-
--- file_upload_app/models.py
+++ file_upload_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from .validators import FileValidator
+
+file_validator = FileValidator(max_size=2500 * 2000,
+                                        content_types=('image/jpeg', 'image/gif', 'image/png'))
+
 
 def user_directory_path(instance, filename):
     """Creates a path string to upload a file to a folder
@@ -10,10 +15,14 @@ def user_directory_path(instance, filename):
     elif isinstance(instance, AlbumFile):
         return f'albums/user_{instance.user.id}/{filename}'
 
+
 class AbstractImage(models.Model):
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to=user_directory_path)
+    image = models.FileField(
+        upload_to=user_directory_path,
+        validators=[file_validator, ]
+    )
     uploaded_at = models.DateTimeField(auto_now=True)
 
     def delete(self, using=None, keep_parents=False):
@@ -26,6 +35,7 @@ class AbstractImage(models.Model):
 
 class AlbumFile(AbstractImage):
     pass
-    
+
+
 class AvatarImage(AbstractImage):
     description = models.CharField(max_length=255, default='')
